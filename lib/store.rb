fde337011a2b35963ee266f46b2fd9b12d54be50
davemgj84 <davemgj@gmail.com>
2020-10-13 17:55:17 +0000

    custom validation

--- lib/store.rb
+++ lib/store.rb
@@ -3,11 +3,13 @@ class Store < ActiveRecord::Base
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
 
-  # validate :at_least_mens_or_womens_apparel
-    
-  # def at_least_mens_or_womens_apparel
-  #   if :mens_apparel == nil && :womens_apparel == nil
-  #     errors.add(:at_least_mens_or_womens_appare, "must be a boolean, true or false")
-  #   end
-  # end
+  # custom validation
+  validate :at_least_mens_or_womens_apparel
+  def at_least_mens_or_womens_apparel
+    if !mens_apparel  && !womens_apparel
+      errors.add(:mens_apparel, "Store must carry at least one of the men's or women's apparel")
+      errors.add(:womens_apparel, "Store must carry at least one of the men's or women's apparel")
+    end
+  end
+
 end
